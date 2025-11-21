c5b60b275c1c9cd93005f2285806010278535793
Mario García <mrgmairena@gmail.com>
2025-11-21 05:51:06 -0600

    Modificar app.routes.ts

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -3,14 +3,12 @@ import { Login } from './modulos/login/componentes/login/login';
 
 export const routes: Routes = [
   {
-    //path: 'login',
     path: 'login',
-    component: Login,
-    pathMatch: 'full'
+    component: Login
   },
   {
     path: 'home',
     loadChildren: () => import('../app/modulos/home/rutas/home.rutas').then((h) => h.HOME_ROUTERS),
   },
- // { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
