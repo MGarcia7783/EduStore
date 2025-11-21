925a3ff79bee0d1351d4044b2b733eac1e0e776b
Mario García <mrgmairena@gmail.com>
2025-11-21 06:24:25 -0600

    Actualizar app.config.ts

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -3,17 +3,12 @@ import { Login } from './modulos/login/componentes/login/login';
 
 export const routes: Routes = [
   {
-    path: '', // RUTA RAÍZ
-    component: Login, // Carga directamente el componente Login
-    pathMatch: 'full'
-  },
-  {
-    path: 'login', // Necesario para que las rutas internas sigan funcionando, aunque la raíz lo cargue.
+    path: 'login',
     component: Login
   },
   {
     path: 'home',
     loadChildren: () => import('../app/modulos/home/rutas/home.rutas').then((h) => h.HOME_ROUTERS),
   },
-  // La línea de redirección (path: '', redirectTo: 'login') DEBE ESTAR COMENTADA O ELIMINADA.
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
