af4450be55bff4b9636e1baeae51940e59c2c009
Mario García <mrgmairena@gmail.com>
2025-11-21 06:11:27 -0600

    Modificar rutas

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -3,12 +3,17 @@ import { Login } from './modulos/login/componentes/login/login';
 
 export const routes: Routes = [
   {
-    path: 'login',
+    path: '', // RUTA RAÍZ
+    component: Login, // Carga directamente el componente Login
+    pathMatch: 'full'
+  },
+  {
+    path: 'login', // Necesario para que las rutas internas sigan funcionando, aunque la raíz lo cargue.
     component: Login
   },
   {
     path: 'home',
     loadChildren: () => import('../app/modulos/home/rutas/home.rutas').then((h) => h.HOME_ROUTERS),
   },
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  // La línea de redirección (path: '', redirectTo: 'login') DEBE ESTAR COMENTADA O ELIMINADA.
 ];
